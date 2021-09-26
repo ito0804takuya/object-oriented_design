9422656a813250f1b670e4933de755bf22775e0d
ito0804takuya <ito0804takuya@gmail.com>
2021-09-27 08:32:23 +0900

    単一責任のクラス 完了

--- single-responsibility.rb
+++ single-responsibility.rb
@@ -1,12 +1,11 @@
 # 自転車のギア
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring # チェーンリングの歯数
     @cog = cog # コグの歯数
-    @rim = rim # リム(タイヤの内側の金属部分)の直径
-    @tire = tire # タイヤの厚み
+    @wheel = wheel # wheelオブジェクト
   end
 
   # ギアの比率(= ペダル1回転に対する車輪の回転数)
@@ -14,12 +13,36 @@ class Gear
     chainring / cog.to_f # 浮動小数点へ変換
   end
 
-  # ギアインチ(ギアと車輪の大きさが異なっても比較できる)
+  # ギアインチ(ギアと車輪の大きさが異なっても比較できる基準)
   def gear_inches
-    # ギア比率 * タイヤの直径(リム+タイヤの厚み)
-    ratio * (rim + (tire * 2))
+    # ギア比率 * タイヤの直径
+    ratio * wheel.diameter
   end
 end
 
-puts Gear.new(52, 11).ratio # このままでは、初期値(引数)が足りないのでエラーになる
-puts Gear.new(30, 27).ratio
+# 自転車の車輪
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim # リム(タイヤの内側の金属部分)の直径
+    @tire = tire # タイヤの厚み
+  end
+
+  # タイヤの直径
+  def diameter
+    rim + (tire * 2) # 直径 = リム+タイヤの厚み
+  end
+
+  # タイヤの円周
+  def circumference
+    diameter * Math::PI # 直径 * π
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference
+
+puts Gear.new(52, 11, @wheel).gear_inches
+
+puts Gear.new(52, 11).ratio
